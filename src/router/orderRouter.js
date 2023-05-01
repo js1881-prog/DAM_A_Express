5d7033c1043d26889b3d8e364feab40b7fe390ae
padonanXD <98295182+padonan@users.noreply.github.com>
2023-05-02 03:40:15 +0900

    Update orderRouter.js

--- src/router/orderRouter.js
+++ src/router/orderRouter.js
@@ -1,18 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
-const Order = require('../dao/orderdao/mongoose/model/orderModel.js');
-const {
-  Product,
-} = require('../dao/productdao/mongoose/model/productModel.js');
 const orderController = require('../controller/orderController.js');
-const SoldProduct = require('../dao/soldproductdao/mongoose/model/soldproductModel.js');
-const UserDAO = require('../dao/userdao/userDAO.js');
-const {
-  validateOrder,
-} = require('../middleware/validate/schema/orderValidate.js');
-const User = require('../dao/userdao/mongoose/model/userModel.js');
-const buildResponse = require('../util/response/buildResponse.js');
+const { validateOrder } = require('../middleware/validate/schema/orderValidate.js');
 
 // /order/
 router.post('/',
